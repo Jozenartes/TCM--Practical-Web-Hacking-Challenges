d31314e0365b9739d3f724ec76e3dc97c5dde37d
Jozenartes <jozenarts12@gmail.com>
2025-05-26 14:21:52 +0100

    Instruction on how to run the script

--- 04-SSRF/lab-02/ssrf-lab-02.py
+++ 04-SSRF/lab-02/ssrf-lab-02.py
@@ -3,3 +3,12 @@ import sys
 import urllib3 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+proxies = {'https': 'http//127.0.0.1:8080', 'http': 'http//127.0.0.1:8080'}
+
+def main():
+    if len(sys.argv) != 2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+
+if __name__ == "__main__":
+    main()
