73131bcf6d30f55ab7290fa75b39b1bebc4c105d
Jozenartes <jozenarts12@gmail.com>
2025-06-05 20:49:46 +0100

    Setting main method

--- 05-SQL-Injection/Lab-01/SQL-01.py
+++ 05-SQL-Injection/Lab-01/SQL-01.py
@@ -4,3 +4,15 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'https': 'http://127.0.0.1', 'http': 'http://127.0.0.1'}
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1].strip()
+    except IndexError:
+        print("[-] Usage: %s <url>" % sys.argv[0])
+        print("[-] Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+        print("[+] Dumping the usernames and passwords")
+        if not exploit_sqli_users_table(url):
+            print("[-] Did not find administrator password")
