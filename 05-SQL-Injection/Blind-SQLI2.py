34c247340116cfa2382ed104ae5eb8783dd5d77d
Jozenartes <jozenarts12@gmail.com>
2025-06-16 14:09:35 +0100

    Added libraries and proxies

--- 05-SQL-Injection/Blind-SQLI2.py
+++ 05-SQL-Injection/Blind-SQLI2.py
@@ -1,4 +1,9 @@
+import sys
 import requests
-import string
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning())
+
+#Sending the requests to burp, to able to debug, incase of an error.
+proxies = {'http':'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
