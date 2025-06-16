51c995f06f503bd350bf21c6daea89da156684ff
Jozenartes <jozenarts12@gmail.com>
2025-06-16 14:10:26 +0100

    main method

--- 05-SQL-Injection/Blind-SQLI2.py
+++ 05-SQL-Injection/Blind-SQLI2.py
@@ -7,3 +7,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning())
 #Sending the requests to burp, to able to debug, incase of an error.
 proxies = {'http':'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
+
+
+if __name__ == "__main__":
+    main()
