8d78044d09fe81887c043efa6986f5f560ee49dd
Jozenartes <jozenarts12@gmail.com>
2025-06-17 14:42:44 +0100

    Added main method

--- 05-SQL-Injection/Lab-02/Blind-SQLI.py
+++ 05-SQL-Injection/Lab-02/Blind-SQLI.py
@@ -8,3 +8,16 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 #Sending the requests to burp, to able to debug, incase of an error.
 proxies = {'http':'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
+#method to launch the payload script correctly
+def main():
+    if len(sys.argv) != 2:
+        print("[+] Usage %s <url>" % sys.argv[0])
+        print("[+] Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+    
+    url = sys.argv[1]
+    print("[+] Checking if tracking cookie is vulnerable to time-based blind SQLi..")
+    blind_sqli_check(url)
+    
+if __name__ == "__main__":
+    main()
