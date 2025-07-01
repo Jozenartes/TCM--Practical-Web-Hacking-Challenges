6b36e2906998c9763c8e1929585b5a13fde36b65
Jozenartes <jozenarts12@gmail.com>
2025-07-01 16:18:29 +0100

    Added main function

--- 05-SQL-Injection/Lab-03/NoSQl.py
+++ 05-SQL-Injection/Lab-03/NoSQl.py
@@ -1,7 +1,20 @@
+import sys
 import requests
 import string
 import urllib3
+import urllib
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'https': 'http://127.0.0.1:8080', 'http': 'http://127.0.0.1:8080'}
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("[+] Usage %s <url>" % sys.argv[0])
+        print("[+] Example: %s www.example.com" % sys.argv[0])
+
+
+
+if __name__ == "__main__":
+    main()
