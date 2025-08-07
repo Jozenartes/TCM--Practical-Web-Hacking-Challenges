57fd085f19d416056c8ac894503af6ed4ff859dd
Jozenartes <jozenarts12@gmail.com>
2025-08-07 11:30:26 +0100

    Added main function

--- 06-File Inclusion/Lab-05/Directory-traveseral-lab05.py
+++ 06-File Inclusion/Lab-05/Directory-traveseral-lab05.py
@@ -5,3 +5,17 @@ urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
 
 proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
 
+
+def main():
+    if len(sys.argv) != 2:
+        print('[-] oops!')
+        print('[+] Usage: %s url' % sys.argv[0])
+        print('[+] Example: %s + www.example.com' % sys.argv[0])
+        sys.exit(-1)
+
+    url = sys.argv[1]
+    print('[+] Exploiting Path traversal vulnerability')
+    path_traversal_exploit(url)
+
+if __name__=='__main__':
+    main()
