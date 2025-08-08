a55ac280b297b6fa81ebbc04579d548f291d5e30
Jozenartes <jozenarts12@gmail.com>
2025-08-08 08:03:27 +0100

    Added libraries and proxies

---
+++ 06-File Inclusion/Lab-06/Directory-traveseral-lab06.py
@@ -0,0 +1,6 @@
+import sys
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
+
+proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
