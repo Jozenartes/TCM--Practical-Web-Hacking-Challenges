9d915385cb299f0c740f12ab1cffd7aa643bc8e4
Jozenartes <jozenarts12@gmail.com>
2025-08-13 21:43:13 +0100

    Added main function

--- 07-XXE(XML-External-Entity-Injection) /Lab-01/xxe-injection-lab-01.py
+++ 07-XXE(XML-External-Entity-Injection) /Lab-01/xxe-injection-lab-01.py
@@ -5,3 +5,16 @@ urllib3.disable_warnings(urllib3.exceptions.SecurityWarning)
 
 proxies = {'http':'http://127.0.0.1:8080','https':'http://127.0.0.1:8080'}
 
+def main():
+    if len(sys.argv) != 2:
+        print('[Oops]')
+        print('[+] Usage: %s <url>' % sys.argv[0])
+        print('[+] Example: %s www.example.com' % sys.argv[0])
+        sys.exit(-1)
+
+        s = requests.Session()
+        url = sys.argv[1]
+        exploit_xxe(s,url)
+
+if __name__ == '__main__':
+    main()
